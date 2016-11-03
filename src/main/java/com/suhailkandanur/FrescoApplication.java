2baee61101262522202549e91e6f1017f5b11815
suhail kandanur <ksuhail7@gmail.com>
2016-11-03 17:02:02 -0400

    gradle files

---
+++ src/main/java/com/suhailkandanur/FrescoApplication.java
@@ -0,0 +1,12 @@
+package com.suhailkandanur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrescoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrescoApplication.class, args);
+	}
+}
