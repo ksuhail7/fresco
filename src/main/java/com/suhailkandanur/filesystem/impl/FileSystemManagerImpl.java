6270696418c2c859fd215731e8751ed49b758580
suhail kandanur <ksuhail7@live.com>
2016-11-21 05:58:29 -0500

    filesystem manager implementation and rabbitmq based messaging

--- src/main/java/com/suhailkandanur/filesystem/impl/FileSystemManagerImpl.java
+++ src/main/java/com/suhailkandanur/filesystem/impl/FileSystemManagerImpl.java
@@ -1,21 +1,27 @@
 package com.suhailkandanur.filesystem.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.suhailkandanur.entity.Document;
 import com.suhailkandanur.entity.DocumentVersion;
 import com.suhailkandanur.entity.Repo;
 import com.suhailkandanur.entity.Store;
 import com.suhailkandanur.filesystem.FileSystemManager;
+import com.suhailkandanur.util.ChecksumUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
 import java.io.IOError;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.FileAttribute;
 import java.util.Objects;
+import java.util.UUID;
+import java.util.zip.Checksum;
 
 import static com.sun.tools.doclint.Entity.rho;
 
@@ -63,23 +69,75 @@ public class FileSystemManagerImpl implements FileSystemManager {
     @Override
     @Transactional
     public void createDocument(Store store, Document document, DocumentVersion version, String srcFileLocation, String storeLocation) throws Exception {
+
+    }
+
+    public void createDocument(String storeLocation, String srcFileLocation, long version, String docId) throws Exception {
+        //required parameters
+        //1. srcFileLocation
+        //2. fileSha1
+        //3. version
+        //4. docIdSha1
+        //5. docId
         //String file, docid, version, docidsha1, storeLocation;
         //create object
-        String fileSha1 = version.getSha1Cksum();
-        Objects.requireNonNull(fileSha1);
-
         final Path srcFilePath = Paths.get(srcFileLocation);
         if (Files.notExists(srcFilePath)) {
             logger.error("source file does not exist at {}", srcFileLocation);
             throw new Exception("source file does not exist at " + srcFileLocation);
         }
+        String fileSha1 = ChecksumUtils.sha1(new File(srcFileLocation));
+        Objects.requireNonNull(fileSha1);
+
+        assert fileSha1.length() == 40;
+        //copy the file
         final Path documentObjectPath = Paths.get(storeLocation, "objects", fileSha1.substring(0, 2), fileSha1.substring(2, 6), fileSha1.substring(6));
         if (Files.exists(documentObjectPath)) {
-            logger.info("document already exists at {} ", documentObjectPath.toString());
+            logger.warn("document already exists at {} ", documentObjectPath.toString());
         } else {
+            if(Files.notExists(documentObjectPath.getParent())) Files.createDirectories(documentObjectPath.getParent());
             Files.copy(srcFilePath, documentObjectPath);
         }
 
+        //create document metadata
+        String docIdSha1 = ChecksumUtils.sha1(docId);
+        Objects.requireNonNull(docIdSha1);
+        assert docIdSha1.length() == 40;
+        final Path docMetadataPath = Paths.get(storeLocation, "documents", docIdSha1.substring(0, 2), docIdSha1.substring(2, 6), docIdSha1.substring(6));
+        if(Files.notExists(docMetadataPath))
+            Files.createDirectories(docMetadataPath);
+        final Path versionPath = docMetadataPath.resolve(Long.toString(version));
+        if (Files.exists(versionPath)) {
+            logger.error("document version {} already exists at path, unable to create new", version, versionPath.toString());
+            throw new Exception("document version already exists");
+        }
 
+        ObjectMapper mapper = new ObjectMapper();
+        final ObjectNode objectNode = mapper.createObjectNode();
+        objectNode.put("docid", docId);
+        objectNode.put("docid_sha1", docIdSha1);
+        objectNode.put("filename", srcFilePath.getFileName().toString());
+        objectNode.put("object", fileSha1);
+        objectNode.put("filesize", srcFilePath.toFile().length());
+        mapper.writeValue(versionPath.toFile(), objectNode);
+        logger.info("document version successfully created {}", versionPath.toString());
+        final Path currentVersionPath = docMetadataPath.resolve("current");
+        try {
+            Files.createSymbolicLink(currentVersionPath, versionPath);
+        } catch(UnsupportedOperationException ex) {
+            logger.error("unable to create symlink, error: {}", ex.getMessage());
+            throw ex;
+        }
+        //point 'current' to the latest one
+        //this should be an atomic operation
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        String storeLocation = "/Users/suhail/tmp/fresco/fs1/testRepo0/store/0";
+        String srcFileLocation = "/Users/suhail/tmp/yolinux-mime-test.png";
+        String docId = "b6bdef6c-ccf1-4717-804d-dff29a97e0fc";
+        long version = System.currentTimeMillis();
+        new FileSystemManagerImpl().createDocument(storeLocation, srcFileLocation, version, docId);
     }
 }
